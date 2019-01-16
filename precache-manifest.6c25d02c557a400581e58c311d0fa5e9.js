ea37a8729f6fd1170acc376cc06ca091355b73ff
onepo1 <1po@korea.com>
2019-01-17 01:24:58 +0900

    Updates

---
+++ precache-manifest.6c25d02c557a400581e58c311d0fa5e9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c7d07e4de97e43d4ca6a",
+    "url": "/movie_app_react/static/js/runtime~main.c7d07e4d.js"
+  },
+  {
+    "revision": "ab78a04697e42f99d0ae",
+    "url": "/movie_app_react/static/js/main.ab78a046.chunk.js"
+  },
+  {
+    "revision": "d69ca8dab7bc8980a7d8",
+    "url": "/movie_app_react/static/js/1.d69ca8da.chunk.js"
+  },
+  {
+    "revision": "ab78a04697e42f99d0ae",
+    "url": "/movie_app_react/static/css/main.78e82148.chunk.css"
+  },
+  {
+    "revision": "37a51626f7a742addb8415411c314ba4",
+    "url": "/movie_app_react/index.html"
+  }
+];
